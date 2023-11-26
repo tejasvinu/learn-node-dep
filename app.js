347670ef127a25ee51d94224c3f346bf64a18241
Tejas Vinay <tejasvinu@gmail.com>
2023-11-26 12:28:29 +0530

    Add express-cors middleware

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('./config/db');
 const cors = require('cors');
+const ecors = require('express-cors');
+
 
 var app = express();
 
@@ -28,6 +30,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 app.use(cors());
+app.use(ecors());
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
