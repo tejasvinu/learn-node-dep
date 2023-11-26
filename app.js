13f8a780e18a46610b682388584472e9a64f0d8f
Tejas Vinay <tejasvinu@gmail.com>
2023-11-26 12:18:18 +0530

    Refactor CORS configuration

--- app.js
+++ app.js
@@ -27,20 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-const corsOptions ={
-  origin:'http://localhost:3000', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
-app.use(function (req, res, next) {
-res.setHeader('Access-Control-Allow-Origin', '*');
-res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-res.setHeader('Access-Control-Allow-Credentials', true);
-next();
-});
-
+app.use(cors());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
