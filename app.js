f79f5316111a90d128773712851c97c57c86970b
Tejas Vinay <tejasvinu@gmail.com>
2024-01-14 23:56:10 +0530

    Remove error handling middleware

--- app.js
+++ app.js
@@ -40,15 +40,5 @@ app.use(session({
 
 app.use('/', indexRouter);
 
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
