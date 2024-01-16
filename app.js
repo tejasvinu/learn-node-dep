07c4cfdaf302d784be17124902d243a6d9e63398
Tejas Vinay <tejasvinu@gmail.com>
2024-01-16 16:00:17 +0530

    Remove unnecessary CORS headers

--- app.js
+++ app.js
@@ -24,20 +24,8 @@ app.use(cors({
   origin: 'https://testmindsai.tech',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
-  headers: 'Authorization, Content-Type',
 }));
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', 'https://testmindsai.tech');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization');
-  
-  if (req.method === 'OPTIONS') {
-      res.sendStatus(200);
-    } else {
-      next();
-    }
-});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
